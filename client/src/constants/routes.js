b76465bd85111d7853c4f227fd429019200bbc9e
anupasm <anupa.shyamlal@gmail.com>
2021-06-29 00:21:28 +0100

    constants added

---
+++ client/src/constants/routes.js
@@ -0,0 +1,10 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
+export const ADD_SERVICE = '/add-service';
+export const VIEW_SERVICE = '/view-service';
+export const NOTIFICATIONS = '/notifications';
